394b3394beb99298230c904d26b1db8d95f40d67
Lucas Azevedo <lucas.elaio@gmai.com>
2026-03-02 10:37:19 -0500

    otimizado metodo de exibir imagens

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "target.scene7.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dw.carters.com",
+      },
+    ],
   },
 };
 
